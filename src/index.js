02fa3519e9ae8c2dc9984514b7709f68c24177b4
ihamza494 <ihamza494@gmail.com>
2021-11-03 21:19:18 +0500

    Greeting application sync with Time

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+var time = new Date()
+var hour = time.getHours()
+let greeting = ''
+const cssStyle = {}
+
+if( hour >= 3 && hour < 12){
+  greeting = 'Good Morning';
+  cssStyle.color = "Green";
+}
+else if( hour >= 12 && hour < 6){
+  greeting = 'Good Afternoon';
+  cssStyle.color = "Orange";
+}
+else{
+  greeting= ' Good night';
+  cssStyle.color = "Black";
+}
+
+function Greetingbox(props){
+  return(
+    <div className='greeting__box'>
+        <h3> Hi Sir,</h3>
+        <p style={cssStyle}> {props.greet}</p> 
+    </div>
+  );
+}
+
+
+ReactDOM.render(<Greetingbox x ={hour} greet={greeting} />, document.getElementById('root'))
